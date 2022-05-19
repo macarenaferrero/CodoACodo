eda75f50c7be7e4da0c691a174e9338549cb3780
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2022-05-19 13:25:04 -0300

    ok form

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     document.getElementsByName("validarForm").addEventListener('submit', validarForm); 
-//   });
 
+function validarForm() {
+    if (document.formulario.nc.value.length == 0) {
+        alert("Tiene que escribir su nombre")
+        document.formulario.nc.focus()
+        return 0;
+    }
 
-// function validarForm() {    
-//     var valorEmail = document.getElementById("email").value;
-//     var valorNombre = document.getElementById("nc").value;
-//     var valorComentario = document.getElementById("comentario").value;
-//     if(valorEmail.length == 0 || valorNombre.length == 0 || valorComentario.length == 0){
-//         alert("Debe completar todos los campos.");
-//     }
-//     alert("Gracias por contactarme.");
-// }
+    email = document.formulario.email.value
+    if (email.length == 0) {
+        alert("Tiene que introducir un email válido.")
+        document.formulario.email.focus()
+        return 0;
+    }
 
-// const button = document.querySelector("button");
-// button.onclick = validarForm();
+    if (document.formulario.comentario.value.length == 0) {
+        alert("No puede enviar un mensaje vacío.")
+        document.formulario.comentario.focus()
+        return 0;
+    }
 
-        (function(){
-            var formulario = document.getElementById("formulario");
-            var boton = document.getElementById("btn");
-
-            var validarNombre = function(e){
-                if (formulario.nc.length == 0){
-                    alert("Completa el campo nombre");
-                    e.preventDefault();
-                }
-            };
-
-            var validarEmail = function(e){
-                if (formulario.email.length == 0){
-                    alert("Completa el campo Email");
-                    e.preventDefault();
-                }
-            };
-            
-            var validarComentario = function(e){
-                if(formulario.comentario.length == 0){
-                    e.preventDefault();
-                    }
-            };
-
-            var validarForm = function(e){
-                validarNombre(e);
-                validarEmail(e);
-                validarComentario(e);
-            };
-            formulario.addEventListener("submit", validarForm);
-        }())
+    alert("Muchas gracias por enviar el formulario");
+    document.formulario.submit();
+}
