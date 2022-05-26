731bd7d11d83f99f745bc3275f164a084f169934
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2022-05-26 01:43:14 -0300

    mejora vista imagenes

--- index.js
+++ index.js
@@ -24,3 +24,32 @@ function validarForm() {
     document.formulario.submit();
 
 }
+
+var modal = document.getElementById('popUp');
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var img4 = document.getElementById('img4');
+var img5 = document.getElementById('img5');
+var img6 = document.getElementById('img6');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img1.onclick = crearPopUp;
+img2.onclick = crearPopUp;
+img3.onclick = crearPopUp;
+img4.onclick = crearPopUp;
+img5.onclick = crearPopUp;
+img6.onclick = crearPopUp;
+
+function crearPopUp() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
