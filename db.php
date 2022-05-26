9f4fef21b89527cb992bea12d13da56f1fe6c81b
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2022-05-26 18:51:56 -0300

    se añade campo Fecha

--- db.php
+++ db.php
@@ -1,21 +1,23 @@
 <?php
 
 function conn(){
+date_default_timezone_set('America/Argentina/Buenos_Aires');
 
-    $hostname = "localhost";
-    $usuariodb = "root";
-    $passworddb = "";
-    $dbname = "contactoloana";
+    $hostname = 'localhost';
+    $usuariodb = 'id18986582_root';
+    $passworddb = 'Lw2qH(k$TQf+Q5G+';
+    $dbname = 'id18986582_contactoloana';
 
     try {
-        $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $usuariodb, $passworddb);
-        $conn->exec("SET CHARACTER SET utf8");
-        echo "Conexión exitosa";
-        return $conn;
-        } catch (PDOException $e) {
-            print "Error: " . $e->getMessage();
-            die();
-        }
+    $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $usuariodb, $passworddb);
+    $conn->exec("SET CHARACTER SET utf8");
+    echo "Conexión exitosa";
+    return $conn;
+    } catch (PDOException $e) {
+        print "Error: " . $e->getMessage();
+        die();
+    }
+    
 }
 
 
--- enviar.php
+++ enviar.php
@@ -1,17 +1,19 @@
 <?php 
-	require_once './db.php';
+require_once './db.php';
 
-	$nombre = $_POST['nc'];
-	$email = $_POST['email'];
-    $comentario = $_POST['comentario'];
+$nombre = $_POST['nc'];
+$email = $_POST['email'];
+$comentario = $_POST['comentario'];
+$fecha = date('Y-m-d H:i:s');
 
-	echo ' <h2>¡Gracias por contactarte ' .$nombre .'!</h2>';
-	
- 	$conectar=conn();
-	
-	$consulta = $conectar->prepare("INSERT INTO mensajes (nombre_completo, email, comentario) VALUES (:nombre_completo, :email, :comentario)");
-        $consulta->bindValue(':nombre_completo', $nombre, PDO::PARAM_STR);
-        $consulta->bindValue(':email', $email, PDO::PARAM_STR);
-        $consulta->bindValue(':comentario', $comentario, PDO::PARAM_STR);
-        $consulta->execute();
+echo ' <h2>¡Gracias por contactarte ' .$nombre .'!</h2>';
+
+	$conectar=conn();
+
+$consulta = $conectar->prepare("INSERT INTO mensajes (nombre_completo, email, comentario, fecha) VALUES (:nombre_completo, :email, :comentario, :fecha)");
+	$consulta->bindValue(':nombre_completo', $nombre, PDO::PARAM_STR);
+	$consulta->bindValue(':email', $email, PDO::PARAM_STR);
+	$consulta->bindValue(':comentario', $comentario, PDO::PARAM_STR);
+	$consulta->bindValue(':fecha', $fecha, PDO::PARAM_STR);
+	$consulta->execute();
  ?>
